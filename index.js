592b2b977e2219aba00425cd35ea576a825c6a72
tonytojo <johansson.andersson@telia.com>
2021-10-30 10:10:18 +0200

    add comment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//DB_CONNECT = mongodb://localhost:27017/auth-jwt;
-//DB_CONNECT = mongodb+srv://Tony:Pissen30060@tonyscluster.qwqe4.mongodb.net/auth-jwt-tony?retryWrites=true&w=majority
+// DB_CONNECT = mongodb://localhost:27017/auth-jwt;
+// DB_CONNECT = mongodb+srv://Tony:Pissen30060@tonyscluster.qwqe4.mongodb.net/auth-jwt-tony?retryWrites=true&w=majority
 //Import some npm package
 const express = require('express');
 const app = express();
